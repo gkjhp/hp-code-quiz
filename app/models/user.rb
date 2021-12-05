a74ff70e2227546c9dcfeaafd65a93808b169f61
Greg Joyce <gkjoyce@gmail.com>
2021-12-05 11:49:29 -0800

    Add idea for removing ID sequences

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,19 @@ class User < ApplicationRecord
     hash = Digest::MD5.hexdigest(email)
     "https://www.gravatar.com/avatar/#{hash}"
   end
+
+  # Thought: in production, posts and threads could use some kind of lookup hash
+  # so we're not throwing around DB ID's
+  #
+  # this would be like an after_save hook
+  #
+  # Usage: Xyz.find_by(safe_hash: params['safe_hash'])
+  #
+  # def rehash
+  #   # md5 is probably fine here, better hash is better
+  #
+  #   new_hash = Digest::MD5("#{id} #{created_at} #{email}")
+  #   or_why_not_do_this_after_create_only = Digest::MD5("#{id} #{created_at} #{Math::random}")
+  #   update_columns(safe_hash: new_hash)
+  # end
 end
